515f9e2e52034bee6f1e1b3d955069b40640e8aa
bex3 <Rebeccam.bearden@gmail>
2016-08-28 16:24:40 -0400

    String reverser works

--- src/com/tiy/networking/StringReverser.java
+++ src/com/tiy/networking/StringReverser.java
@@ -9,11 +9,21 @@ public class StringReverser {
 
     public static void main(String[] args) {
         Scanner inputScanner = new Scanner(System.in);
-        //String testString = String.valueOf(inputScanner.nextLine());
-        String testString = "something";
+        System.out.println("Please enter a word that you would like to have reversed by the magical work reverser!");
+        String testString = String.valueOf(inputScanner.nextLine());
+        //String testString = "something";
+        System.out.println("You input " + testString);
         char[] charArray = testString.toCharArray();
-        for(int counter = charArray.length -1; counter < 0; counter --) {
-            System.out.print(charArray[counter]);
+        //System.out.println(charArray);
+
+        for(int counter = charArray.length -1; counter >= 0; counter --) {
+           System.out.print(charArray[counter]);
+
         }
+
+
+
+
     }
+
 }
